f2ab9e2573af5ea4ac027283393da3fd5420fb55
839756 <839756@unizar.es>
2023-09-26 23:14:21 +0200

    Avance

--- SD_23_24_Practica1EntregadoAlumnos/CodigoEjemploEsqueletoP1/practica1/cmd/server-draft/c-s_secuencial.go
+++ SD_23_24_Practica1EntregadoAlumnos/CodigoEjemploEsqueletoP1/practica1/cmd/server-draft/c-s_secuencial.go
@@ -1,59 +1,76 @@
 
- package main
+package main
 
- import (
-	 //"encoding/gob"
-	 "log"
- 
-	 "net"
-	 "practica1/com"
- )
- 
- // PRE: verdad = !foundDivisor
- // POST: IsPrime devuelve verdad si n es primo y falso en caso contrario
- func isPrime(n int) (foundDivisor bool) {
-	 foundDivisor = false
-	 for i := 2; (i < n) && !foundDivisor; i++ {
-		 foundDivisor = (n%i == 0)
-	 }
-	 return !foundDivisor
- }
- 
- // PRE: interval.A < interval.B
- // POST: FindPrimes devuelve todos los números primos comprendidos en el
- //
- //	intervalo [interval.A, interval.B]
- func findPrimes(interval com.TPInterval) (primes []int) {
-	 for i := interval.Min; i <= interval.Max; i++ {
-		 if isPrime(i) {
-			 primes = append(primes, i)
-		 }
-	 }
-	 return primes
- }
- 
- // COMPLETAR EL SERVIDOR  .....
- func main() {
- 
+import (
+"encoding/gob"
+"log"
+"fmt"
+"net"
+"practica1/com"
+)
+
+// PRE: verdad = !foundDivisor
+// POST: IsPrime devuelve verdad si n es primo y falso en caso contrario
+func isPrime(n int) (foundDivisor bool) {
+	foundDivisor = false
+	for i := 2; (i < n) && !foundDivisor; i++ {
+		foundDivisor = (n%i == 0)
+	}
+	return !foundDivisor
+}
+
+// PRE: interval.A < interval.B
+// POST: FindPrimes devuelve todos los números primos comprendidos en el
+//
+//	intervalo [interval.A, interval.B]
+func findPrimes(interval com.TPInterval) (primes []int) {
+	for i := interval.Min; i <= interval.Max; i++ {
+		if isPrime(i) {
+			primes = append(primes, i)
+		}
+	}
+	return primes
+}
+
+// COMPLETAR EL SERVIDOR  .....
+func main() {
+
 	CONN_TYPE := "tcp"
 	endpoint := ":30000"
 
-	requestTimeChan := make(chan com.TimeCommEvent)
-	replayTimeChan := make(chan com.TimeCommEvent)
- 	 
-	for {
-		listener, err := net.Listen(CONN_TYPE, endpoint)
-		com.CheckError(err)
-	
-		log.SetFlags(log.Lshortfile | log.Lmicroseconds)
+	listener, err := net.Listen(CONN_TYPE, endpoint)
+	com.CheckError(err)
 
-		log.Println("***** Listening for new connection in endpoint ", endpoint)
+	log.SetFlags(log.Lshortfile | log.Lmicroseconds)
+
+	log.Println("***** Listening for new connection in endpoint ", endpoint)
+
+	for {
 		conn, err := listener.Accept()
-				
-		listener -> primo
-		log.Println(primo)
+		com.CheckError(err)
 
 		defer conn.Close()
+		
+		decoder := gob.NewDecoder(conn)
+		encoder := gob.NewEncoder(conn)
+
+		var request com.Request
+		err = decoder.Decode(&request)
+		com.CheckError(err)
+
+		if (request.Id == -1) {
+			fmt.Println("Received end request from client.")
+			return
+		}
+
+		// Process the request
+		reply := com.Reply {
+			Id: request.Id,
+			Primes: findPrimes(request.Interval),
+		}
+
+		// Send the reply back to the client
+		err = encoder.Encode(reply)
 		com.CheckError(err)
 	}	 
- }
+}
