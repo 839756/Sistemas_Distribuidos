cf64f5f593384dff299a052835dc755dcd2ae636
839756 <839756@unizar.es>
2023-09-29 10:43:52 +0200

    El ultimo cambio que hago ya, esto yharto

--- SD_23_24_Practica1EntregadoAlumnos/CodigoEjemploEsqueletoP1/practica1/liberar.sh
+++ SD_23_24_Practica1EntregadoAlumnos/CodigoEjemploEsqueletoP1/practica1/liberar.sh
@@ -3,15 +3,24 @@
 # Define las direcciones IP de las máquinas remotas
 remote_ips=("192.168.3.10" "192.168.3.11" "192.168.3.12")
 
-# Define el usuario
+# Captura el nombre de usuario actual de la terminal
 remote_user="$USER"
 
 # Define el puerto que deseas verificar y matar
-port_to_kill=29120
+port_to_check=29120
 
-# Recorre las direcciones IP y ejecuta el comando para matar el proceso si es necesario
+# Recorre las direcciones IP y ejecuta el comando para matar los procesos si es necesario
 for ip in "${remote_ips[@]}"; do
-  # Utiliza SSH para conectarte a la máquina remota y ejecutar el comando
-  ssh "$remote_user@$ip" "pkill -f :$port_to_kill"
-  echo "Proceso en $ip en el puerto $port_to_kill ha sido terminado."
+  # Utiliza SSH para conectarte a la máquina remota y ejecutar el comando lsof
+  ssh "$remote_user@$ip" "lsof -i :$port_to_check" > /tmp/processes_to_kill
+  # Verifica si se encontraron procesos en el puerto
+  if [ -s /tmp/processes_to_kill ]; then
+    # Extrae los IDs de proceso de los resultados y los mata
+    pids=$(awk '$1 != "COMMAND" {print $2}' /tmp/processes_to_kill)
+    ssh "$remote_user@$ip" "kill -9 $pids"
+    echo "Procesos en $ip en el puerto $port_to_check han sido terminados."
+  else
+    echo "No se encontraron procesos en $ip en el puerto $port_to_check."
+  fi
 done
+
