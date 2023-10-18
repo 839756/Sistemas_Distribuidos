4db0561a31482aa2a95a17a24db188ad160f2720
ega471 <115021226+ega471@users.noreply.github.com>
2023-10-18 16:37:00 +0200

    Update barrera.go

--- SD_23_24_Practica2EntregadoAlumnos/CodigoEjemploEsqueletoP2/practica2/barrera.go
+++ SD_23_24_Practica2EntregadoAlumnos/CodigoEjemploEsqueletoP2/practica2/barrera.go
@@ -1,3 +1,13 @@
+/*
+* AUTOR: Jorge Leris Lacort - 845647
+* AUTOR: Andrei Gabriel Vlasceanu - 839756
+* ASIGNATURA: 30221 Sistemas Distribuidos del Grado en Ingeniería Informática
+*			Escuela de Ingeniería y Arquitectura - Universidad de Zaragoza
+* FECHA: octubre de 2021
+* FICHERO: barrera.go
+* DESCRIPCIÓN: Implementación de la barrera distribuida del sistema
+ */
+
 package main
 
 import (
