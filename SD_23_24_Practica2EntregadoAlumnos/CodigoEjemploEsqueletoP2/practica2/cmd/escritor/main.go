7d0c2db1c5a427f5abcdb7876910e32b88263bc2
ega471 <115021226+ega471@users.noreply.github.com>
2023-10-18 16:40:27 +0200

    Update main.go

--- SD_23_24_Practica2EntregadoAlumnos/CodigoEjemploEsqueletoP2/practica2/cmd/escritor/main.go
+++ SD_23_24_Practica2EntregadoAlumnos/CodigoEjemploEsqueletoP2/practica2/cmd/escritor/main.go
@@ -1,3 +1,13 @@
+/*
+* AUTOR: Jorge Leris Lacort - 845647
+* AUTOR: Andrei Gabriel Vlasceanu - 839756
+* ASIGNATURA: 30221 Sistemas Distribuidos del Grado en Ingeniería Informática
+*			Escuela de Ingeniería y Arquitectura - Universidad de Zaragoza
+* FECHA: octubre de 2021
+* FICHERO: main.go
+* DESCRIPCIÓN: Implementación del escritor del sistema
+ */
+
 package main
 
 //Los ficheros deben estar creados antes??  -  Si no hacer una funcion de creacion para el comienzo
