2ec9cfd40d3727cb060f55155237259d2ab66725
839756 <839756@unizar.es>
2023-10-19 17:11:11 +0200

    Avances practica 2

---
+++ Copia_ya_con_4/CodigoEjemploEsqueletoP2/practica2/gestorF/gestorF.go
@@ -0,0 +1,68 @@
+/*
+* AUTOR: Jorge Leris Lacort - 845647
+* AUTOR: Andrei Gabriel Vlasceanu - 839756
+* ASIGNATURA: 30221 Sistemas Distribuidos del Grado en Ingeniería Informática
+*			Escuela de Ingeniería y Arquitectura - Universidad de Zaragoza
+* FECHA: octubre de 2023
+* FICHERO: gestorF.go
+* DESCRIPCIÓN: Implementación de las dos operaciones necesarias para el proceso gestor de ficheros.
+ */
+
+package gestorF
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+//La operacion LeerFichero devuelve el contenido completo del fichero de texto
+func LeerFichero(nombreArchivo string) (string, error) {
+	// Se lee el contenido del archivo especificado por "nombreArchivo".
+	contenido, err := ioutil.ReadFile(nombreArchivo)
+
+	if err != nil {
+		// Si se produjo un error, imprimir un mensaje de error y retornar un string vacío y el error.
+		fmt.Println("Se ha producido un error en la lectura del fichero")
+		return "", err
+	}
+	// Si no hubo errores, convertir el contenido del archivo en una cadena y retornarla junto con un valor nulo de error.
+	return string(contenido), nil
+}
+
+//La operacion EscribirFichero añade al final del fichero de texto un fragmento
+//Ademas la operacion de escritura se tiene que hacer de tal forma que el proceso escritor escribe en su fichero,
+//pero tambien actualiza los ficheros de todos los procesos lectores y escritores, de manera que todas las copias seran iguales.
+func EscribirFichero(nombreArchivo string, fragmento string) {
+
+	// Abrir el archivo en modo escritura, anexando al final o creando si no existe, con persimos
+	// de escritura y lectura para el propietario y el grupo.
+	archivo, err := os.OpenFile(nombreArchivo, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0664)
+	if err != nil {
+		// Si se produce un error al abrir el archivo, mostrar un mensaje y salir del programa.
+		fmt.Println("Se ha producido un error al abrir el fichero")
+		os.Exit(1)
+	}
+
+	// Se asegura de que el archivo se cierre al finalizar la función.
+	defer archivo.Close()
+
+	// Escribir el fragmento en el archivo.
+	_, err = archivo.WriteString(fragmento)
+	if err != nil {
+		// Si se produce un error al escribir en el archivo, mostrar un mensaje y salir del programa.
+		fmt.Println("Se ha producido un error al escribir en el fichero")
+		os.Exit(1)
+	}
+
+}
+
+//La operacion crearFichero se encarga de crear un fichero vacio
+func CrearFichero(nombreArchivo string) {
+	_, err := os.Create(nombreArchivo)
+	if err != nil {
+		// Si se produce un error al crear el archivo, mostrar un mensaje y salir del programa.
+		fmt.Println("Se ha producido un error al crear el fichero")
+		os.Exit(1)
+	}
+}
---
+++ Copia_ya_con_4/CodigoEjemploEsqueletoP2/practica2/receptor/receptor.go
@@ -0,0 +1,73 @@
+/*
+* AUTOR: Jorge Leris Lacort - 845647
+* AUTOR: Andrei Gabriel Vlasceanu - 839756
+* ASIGNATURA: 30221 Sistemas Distribuidos del Grado en Ingeniería Informática
+*			Escuela de Ingeniería y Arquitectura - Universidad de Zaragoza
+* FECHA: octubre de 2023
+* FICHERO: receptor.go
+* DESCRIPCIÓN: Implementación de la función encargada de recibir los mensajes del sistema
+ */
+
+package receptor
+
+import (
+	"log"
+	"practica2/gestorF"
+	"practica2/ms"
+	"practica2/ra"
+	"strconv"
+)
+
+type CheckPoint struct{}
+
+type Text struct {
+	Text string
+	Pid  int
+}
+
+type TextReply struct{}
+
+// Función que actualiza los ficheros de los demás nodos
+func SendText(message *ms.MessageSystem, numMessage int, me int) {
+	mensaje := strconv.Itoa(numMessage) + " "
+	log.Printf("Envío para que copien %s\n", mensaje)
+	for i := 1; i <= ra.LE; i++ {
+		if i != me {
+
+			message.Send(i, Text{mensaje, me})
+		}
+	}
+}
+
+// Responde al que ha enviado el texto que lo ha copiado de forma satisfactoria
+func SendReplyToTxt(message *ms.MessageSystem, to int) {
+	log.Printf("Hay que mandar una respueta a %d\n", to)
+	message.Send(to, TextReply{})
+	log.Printf("Se ha mandado la respuesta a %d\n", to)
+}
+
+// Espera a que los demás nodos hayan copiado.
+func WaitForReply(chtxt chan bool, numEsperar int) {
+	for i := numEsperar; i > 0; i-- {
+		<-chtxt
+	}
+}
+
+func Receptor(msg *ms.MessageSystem, chreq chan ra.Request, chrep chan ra.Reply, chCheck chan bool, chtxt chan bool, file string) {
+	for {
+		mensaje := msg.Receive()
+		switch tipo := mensaje.(type) {
+		case ra.Request:
+			chreq <- tipo
+		case ra.Reply:
+			chrep <- tipo
+		case CheckPoint:
+			chCheck <- true
+		case Text:
+			gestorF.EscribirFichero(file, tipo.Text)
+			SendReplyToTxt(msg, tipo.Pid)
+		case TextReply:
+			chtxt <- true
+		}
+	}
+}
