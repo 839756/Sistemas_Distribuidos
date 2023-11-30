cd61e65caa071e090d5806b6dcf7e9ce0998d08a
839756 <839756@unizar.es>
2023-11-30 23:13:53 +0100

    Jesucristo por favor no me castigues

--- practica5/Kubernetes/start.sh
+++ practica5/Kubernetes/start.sh
@@ -17,13 +17,13 @@ CGO_ENABLE=0 go build -o ../../../Dockerfiles/cliente/cliente main.go
 
 cd ../../../Dockerfiles/cliente
 # Build Docker image for servidor and push it
-docker build -t localhost:5000/servidor:latest .
-docker push localhost:5000/servidor:latest
+docker build -t localhost:5001/servidor:latest .
+docker push localhost:5001/servidor:latest
 
 cd ../cliente
 # Build Docker image for cliente and push it
-docker build -t localhost:5000/cliente:latest .
-docker push localhost:5000/cliente:latest
+docker build -t localhost:5001/cliente:latest .
+docker push localhost:5001/cliente:latest
 
 cd ../..
 kubectl create -f statefulset_go.yaml
