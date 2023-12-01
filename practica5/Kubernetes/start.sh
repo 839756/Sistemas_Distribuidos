ec9b77d91a5fb72287869de1abb086678eaa6438
839756 <839756@unizar.es>
2023-12-01 21:59:06 +0100

    Empezar de nuevo

--- practica5/Kubernetes/start.sh
+++ practica5/Kubernetes/start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 kind delete cluster
-./kind-with-registry.sh
-
 docker stop kind-registry
 docker rm kind-registry
+./kind-with-registry.sh
 
 rm Dockerfiles/servidor/servidor
 rm Dockerfiles/cliente/cliente
@@ -17,16 +16,16 @@ CGO_ENABLE=0 go build -o ../../../Dockerfiles/cliente/cliente main.go
 
 cd ../../../Dockerfiles/servidor
 # Build Docker image for servidor and push it
-docker build -t localhost:5001/servidor:latest .
-docker push localhost:5001/servidor:latest
+docker build -t 127.0.0.1:5000/servidor:latest .
+docker push 127.0.0.1:5000/servidor:latest
 
 # docker tag localhost:5001/servidor:latest 839756/sistemas-sistribuidos:servidor
 # docker push 839756/sistemas-sistribuidos:servidor
 
 cd ../cliente
 # Build Docker image for cliente and push it
-docker build -t localhost:5001/cliente:latest .
-docker push localhost:5001/cliente:latest
+docker build -t 127.0.0.1:5000/cliente:latest .
+docker push 127.0.0.1:5000/cliente:latest
 
 # docker tag localhost:5001/cliente:latest 839756/sistemas-sistribuidos:cliente
 # docker push 839756/sistemas-sistribuidos:cliente
